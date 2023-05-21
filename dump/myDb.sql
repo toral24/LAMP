c62a9ba2eb4d062e6310abe4550266de102a919d
unknown <toralsergio23@gmail.com>
2023-05-21 15:01:42 +0200

    2

--- dump/myDb.sql
+++ dump/myDb.sql
@@ -1,34 +1,36 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
--- http://www.phpmyadmin.net
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-01-2023 a las 13:47:35
--- Versión del servidor: 5.6.24-log
--- Versión de PHP: 5.6.8
+-- Tiempo de generación: 01-02-2023 a las 19:52:52
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `tienda`
---
 DROP DATABASE IF EXISTS tienda;
 CREATE DATABASE tienda;
 USE tienda;
+--
+
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `clientes`
 --
 
-CREATE TABLE IF NOT EXISTS `clientes` (
+CREATE TABLE `clientes` (
   `dni` varchar(10) NOT NULL,
   `usuario` varchar(30) DEFAULT NULL,
   `nombre` varchar(30) DEFAULT NULL,
@@ -36,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `clientes` (
   `mail` varchar(50) DEFAULT NULL,
   `nacim` date DEFAULT NULL,
   `pass` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Volcado de datos para la tabla `clientes`
@@ -52,14 +54,14 @@ INSERT INTO `clientes` (`dni`, `usuario`, `nombre`, `apellidos`, `mail`, `nacim`
 -- Estructura de tabla para la tabla `productos`
 --
 
-CREATE TABLE IF NOT EXISTS `productos` (
+CREATE TABLE `productos` (
   `id` int(3) NOT NULL,
   `descripcion` varchar(100) DEFAULT NULL,
   `precio` float(10,2) DEFAULT NULL,
   `imagen` varchar(30) DEFAULT NULL,
   `id_tipo` varchar(30) DEFAULT NULL,
   `cantidad` int(4) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Volcado de datos para la tabla `productos`
@@ -80,10 +82,10 @@ INSERT INTO `productos` (`id`, `descripcion`, `precio`, `imagen`, `id_tipo`, `ca
 -- Estructura de tabla para la tabla `tipo`
 --
 
-CREATE TABLE IF NOT EXISTS `tipo` (
+CREATE TABLE `tipo` (
   `id` int(3) NOT NULL,
   `nombre` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Volcado de datos para la tabla `tipo`
@@ -128,12 +130,15 @@ ALTER TABLE `tipo`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
 -- AUTO_INCREMENT de la tabla `tipo`
 --
 ALTER TABLE `tipo`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
